fa55ca2d2487ce8c3e122646d61c62f54fb3be59
megakaban <nakedant19@gmail.com>
2019-12-01 12:51:19 +0700

    Add getType method

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const getSuccess = todoCreators.getTodo.success(['Todo1', 'Todo2']);
 const getFailure = todoCreators.getTodo.failure('Error');
 const getReset = todoCreators.getTodo.reset();
 
+const plainType = todoCreators.add.getType();
+const requestType = todoCreators.getTodo.getType('request');
+console.log('Raw types:', plainType, requestType);
+
 store.dispatch(add);
 store.dispatch(getRequest);
 store.dispatch(getSuccess);
